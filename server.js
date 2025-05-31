931e9f6d0280b48c881727a21455ecab09aac583
seun-alade <aladeoluwaseunbamidele@gmail.com>
2025-05-31 23:47:41 +0000

    12th commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: "https://newskill-bridge.vercel.app",
+    origin: "https://15.223.230.143", // Replace with your frontend URL
     credentials: true,
   })
 );
@@ -40,5 +40,5 @@ app.use("/api", calendarRoutes);
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () =>
-  console.log(`Server running on https://newskill-bridge.vercel.app:${PORT}`)
+  console.log(`Server running on https://15.223.230.143:${PORT}`)
 );
